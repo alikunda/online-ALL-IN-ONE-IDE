d34a1096b18f128a539a1e3e137e6a8e2fd9bebc
alikunda <76674498+alikunda@users.noreply.github.com>
2021-11-05 19:08:52 -0500

    Compiler, theme, home page, input output working

---
+++ public/js/index.js
@@ -0,0 +1,224 @@
+const runCode = document.querySelector('.run');
+const resetCode = document.querySelector('.reset');
+
+var editor = ace.edit("editor");
+
+editor.setTheme("ace/theme/dracula");
+editor.session.setMode("ace/mode/javascript");
+ace.require("ace/ext/language_tools");
+
+
+editor.setOptions({
+  enableBasicAutocompletion: true,
+  enableSnippets: true,
+  enableLiveAutocompletion: true
+});
+editor.clearSelection();
+/* -------------------------------------OPEN FILE-----------------------------------------*/
+
+
+var modeHolder;
+$(document).ready(function () {
+  $("select.languages").change(function () {
+    let selectedItem = $(this).children("option:selected").text();
+    if (selectedItem == 'Java') {
+      modeHolder = "java";
+    }
+    else if (selectedItem == 'Python') {
+      modeHolder = "py";
+    }
+    else if (selectedItem == 'C++') {
+      modeHolder = "cpp";
+    }
+    else if (selectedItem == 'C') {
+      modeHolder = "c"
+    }
+    else if (selectedItem == 'Swift') {
+      modeHolder = "swift"
+    }
+    console.log("lang: " + modeHolder)
+
+  });
+});
+///////////////////////////////////////////////////////////////////////////////
+
+function executeCode() {
+
+  
+
+  if(modeHolder ==undefined){
+    alert("Please select programming lnaguage first!")
+  }
+
+  else{
+   var input_user = document.getElementById('input').value;
+    const response = new XMLHttpRequest();
+    var data = JSON.stringify({
+      "code": editor.getValue(),
+      "language": modeHolder,
+      "input": input_user
+  
+    });
+    console.log("****"+input_user);
+    document.getElementById('output').innerHTML = "Loading..."
+    response.open("POST", 'https://cors-anywhere-jaagrav.herokuapp.com/https://codexweb.netlify.app/.netlify/functions/enforceCode')
+    response.setRequestHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    response.send(data);
+    response.onload = (e) => {
+      document.getElementById('output').innerHTML = ""
+      console.log(modeHolder)
+      console.log(response.response); // printing the output
+    
+      var x = JSON.parse(response.response);
+      document.getElementById('output').innerHTML = x.output;
+    }
+  }
+}
+////////////////////////////////////////////////////////////////////////////////
+
+// Function to change the mode of the editor as a different language is selected dynamically
+function changeLanguage() {
+  var x = document.getElementById("mode");
+  var modeValue = x.options[x.selectedIndex].value;
+  if (modeValue == "c") {
+    editor.session.setMode("ace/mode/c_cpp");
+    editor.setValue("#include <stdio.h>\n\n\nint main() {\n    // Complete the code.\n    return 0;\n}\n");
+    editor.clearSelection();
+    document.getElementById('langExt').innerHTML = "c";
+    console.log("C");
+  }
+  if (modeValue == "c++") {
+    editor.session.setMode("ace/mode/c_cpp");
+    editor.setValue("#include <iostream>\nusing namespace std;\n\nint main() {\n    // Complete the code.\n    return 0;\n}\n");
+    editor.clearSelection();
+    document.getElementById('langExt').innerHTML = "cpp";
+    console.log("C++");
+  }
+  if (modeValue == "python") {
+    editor.session.setMode("ace/mode/python");
+    editor.setValue("print('hello world')");
+    editor.clearSelection();
+    document.getElementById('langExt').innerHTML = "py";
+    console.log("python");
+  }
+  if (modeValue == "java") {
+    editor.session.setMode("ace/mode/java");
+    editor.setValue("import java.io.*;\n\nclass Main {\n\n    public static void main(String[] args) {\n        // Your code goes here\n   }\n}\n");
+    editor.clearSelection();
+    document.getElementById('langExt').innerHTML = "java";
+    console.log("Java");
+  } if (modeValue == "Swift") {
+    editor.session.setMode("ace/mode/swift");
+    editor.setValue('var myString = "Hello, World!";' 
+    +'\nprint(myString)');
+
+    editor.clearSelection();
+    document.getElementById('langExt').innerHTML = "swift";
+  }
+}
+$(document).ready(function () {
+  $('.selectpicker').selectpicker();
+  $('[data-toggle="tooltip"]').tooltip();
+});
+let count = 1;
+
+const open = docuemnt.querySelector(".open");
+open.addEventListener("click",
+  function openCode(files) {
+    var file = files[0]
+    if (!file) return;
+    var modelist = ace.require("ace/ext/modelist")
+    var modeName = modelist.getModeForPath(file.name).mode
+    editor.session.setMode(modeName)
+    reader = new FileReader();
+    reader.onload = function () {
+      editor.session.setValue(reader.result)
+    }
+    reader.readAsText(file)
+  });
+function elemTarget(e) {
+  e = e || window.event;
+  e.preventDefault();
+  return e.target;
+}
+
+document.addEventListener("click", function (e) {
+  let elem = elemTarget(e);
+
+  if (elem.id == 'download') {
+    saveText();
+  }
+});
+
+function saveText() {
+  let data = editor.getValue();
+  if (modeHolder != "java" && modeHolder != "py" && modeHolder != "cpp" && modeHolder != "c" && modeHolder != "javascript") {
+    console.log('No language selected')
+  }
+  else {
+    let file = 'unititled-' + count + '.' + modeHolder;
+    console.log("Language extension: " + modeHolder);
+    count++;
+
+    let link = document.createElement('a');
+    link.download = file;
+    let blob = new Blob(['' + data + ''], {
+      type: 'text/plain'
+    });
+    link.href = URL.createObjectURL(blob);
+    link.click();
+    URL.revokeObjectURL(link.href);
+  }
+}
+
+//change the theme
+function changeThemes() {
+  var x = document.getElementById("themes");
+  var modeValue = x.options[x.selectedIndex].value;
+  if (modeValue == "twilight") {
+    editor.setTheme("ace/theme/twilight");
+  }
+  if (modeValue == "monokai") {
+    editor.setTheme("ace/theme/monokai");
+  }
+  if (modeValue == "dracula") {
+    editor.setTheme("ace/theme/dracula");
+
+  }
+  if (modeValue == "eclipse") {
+    editor.setTheme("ace/theme/eclipse");
+  }
+  if (modeValue == "ambiance") {
+    editor.setTheme("ace/theme/ambiance");
+  }
+  if (modeValue == "chaos") {
+    editor.setTheme("ace/theme/chaos");
+  }
+}
+$(document).ready(function () {
+  $('.selectpicker').selectpicker();
+  $('[data-toggle="tooltip"]').tooltip();
+});
+
+
+runCode.addEventListener('click', () => {
+  const userCode = editor.getValue();  //get the code from user
+  try {
+    var run = new Function(userCode)();
+
+  } catch {
+    console.error(err);
+  }
+});
+
+
+
+document.getElementById('editor').style.fontSize = '14px';
+resetCode.addEventListener("click", () => {
+  editor.setValue(" Select the language first");
+})
+
+
+
+
+
--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const pageRouter = require('./controller/pages');
+const cors = require('cors');
 const mongoose = require('mongoose');
 
 const app = express();
-
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use("/", pageRouter);
@@ -15,6 +15,6 @@ app.use("/", pageRouter);
 mongoose.connect('mongodb+srv://root:root@cluster0.y21un.mongodb.net/ALL-IN-ONE-IDE?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         app.listen(3000, () => {
-            console.log('MongoDB Atlas is connected and Express server is running...');
+            console.log('Application is connected and Express server is running...');
         });
     });
